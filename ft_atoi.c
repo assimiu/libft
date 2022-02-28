6560de28adf216d852e5e0de443d86ef9c0a4372
assimiu <assimium2@gmail.com>
2022-02-28 13:00:24 -0500

    Alteracão de atoi, isalpha

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,35 +1,29 @@
 
 
-int ft_atoi(char *a)
+int	ft_atoi(const char *str)
 {
-    int     valor;
-    int     sinal;
-    int     entr;
+	int			sign;
+	long long	num;
 
-    valor = 0;
-    sinal = 1;
-    entr = 0;
-    if (a == 0)
-        return (0);
-    while (*a)
-    {
-        if (entr == 1 && (*a > '9' || *a < '0'))
-            return (valor * sinal);
-        if ((*a == '+' || *a == '-') && sinal == -1)
-            return (valor * sinal);
-        if (*a == '-')
-            sinal = -1;
-        if ((*a > '9' || *a < '0') && *a != '-' && *a != '+')
-                if (*a != '\t' && *a != '\n' && *a != ' ')
-                    if (*a != '\f' && *a != '\r' && *a != '\v')
-                        return (valor * sinal);
-        if (*a >= '0' && *a <= '9')
-        {
-            valor = (valor * 10) + (*a - 48);
-            entr = 1;
-        }
-        a ++;
-    }
-	return (valor * sinal);
+	sign = 1;
+	num = 0;
+	while ((*str == 32) || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == 45)
+	{
+		sign *= -1;
+		str++;
+	}
+	else if (*str == 43)
+		str++;
+	while (*str >= 48 && *str <= 57)
+	{
+		num = num * 10;
+		num += (sign * (*(str++) - '0'));
+		if (num > 2147483647)
+			return (-1);
+		if (num < -2147483648)
+			return (0);
+	}
+	return (num);
 }
-
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,8 +1,8 @@
 
-int ft_isalpha(int a)
+int	ft_isalpha(int c)
 {
-	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-        return (1);
-	return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
-//resto
